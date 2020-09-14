dceed2ee8580a411eedf983285ba54158b68d255
akgupta-47 <60443592+akgupta-47@users.noreply.github.com>
2020-09-14 13:37:13 +0530

    more validations added

--- models/hospital.js
+++ models/hospital.js
@@ -1,52 +1,66 @@
 const mongoose = require('mongoose');
+// download validator from npm
+const validator = require('validator');
 const Schema = mongoose.Schema;
-
 
 const hospitalSchema = new Schema({
     name: {
         type: String,
-        required: true
+        required: [true, 'A hospital needs a name'],
+        trim: true,
+        maxlength: [40, 'The maximum length of name is 40 characters'],
+        minlength: [10, 'The minimum length of name is 10 characters'],
     },
     state: {
         type: String,
-        required: true
+        required: [true, 'A hospital needs a state'],
     },
     district: {
         type: String,
-        required: true
+        required: [true, 'A hospital needs a district'],
     },
     Tehsil: {
         type: String,
-        required: true
+        required: [true, 'A hospital needs a Tehsil'],
     },
     address: {
         type: String,
-        required: true
+        required: [true, 'A hospital needs a address'],
     },
     type: {
         type: String,
-        required: true,
-        enum: ['Government', 'Private','Semi Government']
+        required: [true, 'A hospital needs a Type'],
+        enum: {
+            values: ['Government', 'Private','Semi Government'],
+            message: 'Type can only be govt. , private, or semi Govt.',
+        }
     },
     totalBedsCount: {
         type: Number,
-        required: true
+        required: [true, 'A hospital needs count of totoal beds'],
     },
     OccupiedBedsCount: {
         type: Number,
-        required: true
+        required: [true, 'A hospital should have data of occupied beds'],
     },
     email:{
         type: String,
-        required: true
+        required: [true, 'A hospital needs a email'],
+        validate: [validator.isEmail, 'Please enter a valid email!!'],
     },
     head: {
         type: String,
-        required: true
+        required: [true, 'A hospital needs a head'],
     },
     Contact: {
         type: String,
-        required: true
+        validate: {
+          validator: function(v) {
+            return /\d{3}-\d{3}-\d{4}/.test(v);
+          },
+          message: props => `${props.value} is not a valid phone number!`
+        },
+        required: [true, 'A hospital needs a Contact'],
     },
     Photo: {
         type: Buffer
@@ -54,7 +68,7 @@ const hospitalSchema = new Schema({
     },
     password: {
         type: String,
-        required: true
+        required: [true, 'A hospital needs a password'],
     }
 });
 
