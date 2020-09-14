5ec06358c0f035c4d3d3c08fc70bf31f2f385ddf
akgupta-47 <60443592+akgupta-47@users.noreply.github.com>
2020-09-14 13:25:20 +0530

    Update user.js

--- models/user.js
+++ models/user.js
@@ -5,39 +5,49 @@ const Schema = mongoose.Schema;
 const user = new Schema({
     name: {
         type: String,
-        required: true
+        required: [true, 'Every User must have a name' ]
     },
     dob: {
         type: String,
-        required: true
+        required: [true, 'Every User must have a date of birth' ]
     },
     aadhaar: {
         type: String,
-        required: true
+        unique: true,
+        minLength: 12,
+        maxLength: 12,
+        validate: {
+          validator: function(v) {
+            return /\d{4}-\d{4}-\d{4}/.test(v);
+          },
+          message: props => `${props.value} is not a valid Adhar number!!`
+        },
+        required: [true, 'Every User must have a Adhar number' ]
     },
     healthid: {
         type: String,
-        required: true
+        required: [true, 'Every User must have a healtId' ]
     },
     state: {
         type: String,
-        required: true
+        required: [true, 'Every User must have a state' ]
     },
     district: {
         type: String,
-        required: true
+        required: [true, 'Every User must have a district' ]
     },
     Tehsil: {
         type: String,
-        required: true
+        required: [true, 'Every User must have a Tehsil' ]
     },
     address: {
         type: String,
-        required: true
+        required: [true, 'Every User must have a address' ]
     },
     privateKey: {
         type: String,
-        required: true
+        unique: true,
+        required: [true, 'Every User must have a private key' ]
     },
     password: {
         type: String,
